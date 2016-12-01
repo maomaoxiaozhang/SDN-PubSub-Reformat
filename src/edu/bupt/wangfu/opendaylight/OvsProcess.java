856cd1420dc9e5f942955b65d392925bb1239f4c
HanB <hanbo0611@gmail.com>
2016-12-01 14:35:51 +0800

    重更新流表生成函数

--- src/edu/bupt/wangfu/opendaylight/OvsProcess.java
+++ src/edu/bupt/wangfu/opendaylight/OvsProcess.java
@@ -1,8 +1,6 @@
 package edu.bupt.wangfu.opendaylight;
 
 import edu.bupt.wangfu.info.device.Controller;
-import org.json.JSONArray;
-import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -12,7 +10,7 @@ import java.io.InputStreamReader;
  */
 public class OvsProcess {
 	public static void addFlow(Controller ctrl, String tpid, String body) {
-		String brName = getBrNameByTpid(ctrl, tpid);
+		String brName = RestProcess.getBrNameByTpid(ctrl, tpid);
 		String cmd = "ovs-ofctl add-flow " + brName + " \"" + body + "\"";
 		try {
 			Runtime.getRuntime().exec(cmd);
@@ -22,7 +20,7 @@ public class OvsProcess {
 	}
 
 	public static void deleteFlows(Controller ctrl, String tpid, String body) {
-		String brName = getBrNameByTpid(ctrl, tpid);
+		String brName = RestProcess.getBrNameByTpid(ctrl, tpid);
 		String cmd = "ovs-ofctl del-flows " + brName + " \"" + body + "\"";
 		try {
 			Runtime.getRuntime().exec(cmd);
@@ -32,7 +30,7 @@ public class OvsProcess {
 	}
 
 	public static String dumpFlows(Controller ctrl, String tpid, String body) {
-		String brName = getBrNameByTpid(ctrl, tpid);
+		String brName = RestProcess.getBrNameByTpid(ctrl, tpid);
 		String cmd = "ovs-ofctl dump-flows " + brName + " \"" + body + "\"";
 		BufferedReader br = null;
 		StringBuilder sb = new StringBuilder();
@@ -55,28 +53,5 @@ public class OvsProcess {
 			}
 		}
 		return sb.toString();
-	}
-
-	public static String getBrNameByTpid(Controller ctrl, String tpid) {
-		String brName = "";
-		String url = ctrl.url + "/restconf/operational/opendaylight-inventory:nodes/node/" + tpid;
-		String body = RestProcess.doClientGet(url);
-		assert body != null;
-		try {
-			JSONObject json = new JSONObject(body);
-			JSONArray node = json.getJSONArray("node");
-			JSONArray node_connector = node.getJSONObject(0).getJSONArray("node-connector");
-			for (int i = 0; i < node_connector.length(); i++) {
-				JSONObject term = node_connector.getJSONObject(i);
-				String portName = term.getString("flow-node-inventory:name");
-				if (portName.startsWith("br")) {
-					brName = portName;
-					break;
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return brName;
 	}
 }
