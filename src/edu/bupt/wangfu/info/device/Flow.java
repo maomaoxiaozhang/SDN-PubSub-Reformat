ebe85c9154c7c8e99deafa32437e9f13f2add0a0
HanB <hanbo0611@gmail.com>
2016-12-06 15:04:24 +0800

    重更新流表生成函数

--- src/edu/bupt/wangfu/info/device/Flow.java
+++ src/edu/bupt/wangfu/info/device/Flow.java
@@ -15,21 +15,20 @@ public class Flow implements Serializable {
 	public String out;
 	public String nw_src; // ipv4 源ip
 	public String nw_dst; // ipv4 目的ip
-	public int dl_type = 0x86DD; // 匹配ipv6必须设置的参数
 	public String ipv6_dst; // ipv6 目的地址
 
 	public String toStringOutput() {
 		if (in != null) { // generateFlow
-			return String.format("table=%d priority=%s dl_type=%d in_port=%s ipv6_dst=%s action=output:%s", table_id, priority, dl_type, in, ipv6_dst, out);
+			return String.format("table=%d priority=%s dl_type=%d in_port=%s ipv6_dst=%s action=output:%s", table_id, priority, 0x86DD, in, ipv6_dst, out);
 		}
 		if (ipv6_dst != null) { // generateNoInPortFlow
-			return String.format("table=%d priority=%s dl_type=%d ipv6_dst=%s action=output:%s", table_id, priority,dl_type, ipv6_dst, out);
+			return String.format("table=%d priority=%s dl_type=%d ipv6_dst=%s action=output:%s", table_id, priority,0x86DD, ipv6_dst, out);
 		}
 		if (nw_src != null) { // generateRestFlow
-			return String.format("table=%d priority=%s nw_src=%s action=output:%s", table_id, priority, nw_src, out);
+			return String.format("table=%d priority=%s dl_type=%d nw_src=%s action=output:%s", table_id, priority,0x0800, nw_src, out);
 		}
 		if (nw_dst != null) {
-			return String.format("table=%d priority=%s nw_dst=%s action=output:%s", table_id, priority, nw_dst, out);
+			return String.format("table=%d priority=%s dl_type=%d nw_dst=%s action=output:%s", table_id, priority, 0x0800, nw_dst, out);
 		}
 		return null;
 	}
@@ -39,10 +38,10 @@ public class Flow implements Serializable {
 	}
 
 	public String toStringEnQueue() {
-		return String.format("table=%d priority=%s in_port=%s ipv6_dst=%s action=enqueue:%s", table_id, priority, in, ipv6_dst, out);
+		return String.format("table=%d priority=%s dl_type=%d in_port=%s ipv6_dst=%s action=enqueue:%s", table_id, priority, 0x86DD, in, ipv6_dst, out);
 	}
 
 	public String toStringDelete() {
-		return String.format("table=%d in_port=%s ipv6_dst=%s action=output:%s", table_id, in, ipv6_dst, out);
+		return String.format("table=%d dl_type=%d in_port=%s ipv6_dst=%s", table_id, 0x86DD, in, ipv6_dst);
 	}
 }
