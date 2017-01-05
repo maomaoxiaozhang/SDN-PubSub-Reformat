98f9b4fc9578635d4091a71db6f32307e960613a
HanB <hanbo0611@gmail.com>
2017-01-05 17:02:45 +0800

    remoteExcuteCommand

--- src/edu/bupt/wangfu/opendaylight/OvsProcess.java
+++ src/edu/bupt/wangfu/opendaylight/OvsProcess.java
@@ -1,9 +1,13 @@
 package edu.bupt.wangfu.opendaylight;
 
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
 import edu.bupt.wangfu.info.device.Controller;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Properties;
 
 /**
  * @ Created by HanB on 2016/11/29.
@@ -12,27 +16,36 @@ public class OvsProcess {
 	public static void addFlow(Controller ctrl, String tpid, String body) {
 		String brName = RestProcess.getBrNameByTpid(ctrl, tpid);
 		String cmd = "ovs-ofctl add-flow " + brName + " " + body;
-		try {
+
+		remoteExcuteCommand(cmd);
+
+		/*try {
 			Runtime.getRuntime().exec(cmd);
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
+		}*/
 	}
 
 	public static void deleteFlows(Controller ctrl, String tpid, String body) {
 		String brName = RestProcess.getBrNameByTpid(ctrl, tpid);
 		String cmd = "ovs-ofctl del-flows " + brName + " " + body;
-		try {
+
+		remoteExcuteCommand(cmd);
+
+		/*try {
 			Runtime.getRuntime().exec(cmd);
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
+		}*/
 	}
 
 	public static String dumpFlows(Controller ctrl, String tpid, String body) {
 		String brName = RestProcess.getBrNameByTpid(ctrl, tpid);
 		String cmd = "ovs-ofctl dump-flows " + brName + " " + body;
-		BufferedReader br = null;
+
+		return remoteExcuteCommand(cmd);
+
+		/*BufferedReader br = null;
 		StringBuilder sb = new StringBuilder();
 		try {
 			Process p = Runtime.getRuntime().exec(cmd);
@@ -51,6 +64,36 @@ public class OvsProcess {
 					e.printStackTrace();
 				}
 			}
+		}
+		return sb.toString();*/
+	}
+
+	public static String remoteExcuteCommand(String cmd) {
+		StringBuilder sb = new StringBuilder();
+		try {
+			JSch jsch = new JSch();
+			Session session = jsch.getSession("root", "10.109.253.3", 22);
+			session.setPassword("123456");
+			Properties config = new Properties();
+			config.put("StrictHostKeyChecking", "no");
+			session.setConfig(config);
+			session.connect();
+			ChannelExec channel = (ChannelExec) session.openChannel("exec");
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(channel.getInputStream()));
+
+			channel.setCommand(cmd);
+			channel.connect();
+
+			String msg;
+			while ((msg = in.readLine()) != null) {
+				sb.append(msg).append("\n");
+			}
+
+			channel.disconnect();
+			session.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		return sb.toString();
 	}
